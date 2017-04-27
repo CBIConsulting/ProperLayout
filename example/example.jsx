978160f1f7384f93f45b4661494a684f3bf5ee35
orballo <asrielo@gmail.com>
2017-04-27 13:46:46 +0200

    Perf OK

--- example/example.jsx
+++ example/example.jsx
@@ -1,104 +1,139 @@
-'use strict';
+'use strict'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Layout, Section, Row, Col } from '../src/ProperLayout';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Layout, Section, Row, Col } from '../src/ProperLayout'
 
-let main = document.createElement('div');
-main.id = 'main';
-document.body.appendChild(main);
-
+let main = document.createElement('div')
+main.id = 'main'
+document.body.appendChild(main)
+/**
+ * [style description]
+ * @type {Object}
+ */
+class Content extends React.Component {
+  render () {
+    return (
+      <div style={{height: '100%'}}>
+        <div style={{
+          height: '300px',
+          border: '1px solid red'
+        }}>
+          div1
+        </div>
+        <div style={{
+          height: '100%',
+          border: '3px solid limegreen'
+        }}>
+          div2
+        </div>
+      </div>
+    )
+  }
+}
+/**
+ * [cases description]
+ * @type {Object}
+ */
 const cases = {
-	simple: (
-		<Layout>
-			<Section />
-			<Section />
-		</Layout>
-		),
-	fixedRow: (
-		<Layout type='rows'>
-			<Section size='300px'/>
-			<Section />
-		</Layout>
-	),
-	fixedCol: (
-		<Layout>
-			<Section size='300px'/>
-			<Section />
-		</Layout>
-	),
-	rows: (
-		<Layout type='rows'>
-			<Row />
-			<Row />
-		</Layout>
-	),
-	columns: (
-		<Layout type='columns'>
-			<Col />
-			<Col />
-		</Layout>
-	),
-	width: (
-		<Layout>
-			<Section gravity={-1} width={300} />
-			<Section />
-		</Layout>
-	),
-	height: (
-		<Layout type='rows'>
-			<Section gravity={-1} height={300} />
-			<Section />
-		</Layout>
-	),
-	common: (
-		<Layout type='rows'>
-			<Section size='80px' />
-			<Section>
-				<Layout>
-					<Section size='20%' />
-					<Section>
-						<Layout type='rows' mode='spaced'>
-							<Section>
-								<Layout mode='spaced'>
-									<Section size='300px' />
-									<Section />
-									<Section size='300px' />
-								</Layout>
-							</Section>
-							<Section />
-							<Section />
-						</Layout>
-					</Section>
-				</Layout>
-			</Section>
-		</Layout>
-	),
-	singleLayout: (
-		<Layout />
-	),
-	singleSection: (
-		<Section />
-	),
-	tenColumns: (
-		<Layout>
-			<Section />
-			<Section />
-			<Section size='20px'/>
-			<Section />
-			<Section />
-			<Section />
-			<Section />
-			<Section />
-			<Section />
-			<Section />
-		</Layout>
-	)
-};
+  simple: (
+    <Layout>
+      <Section />
+      <Section />
+    </Layout>
+    ),
+  fixedRow: (
+    <Layout type='rows'>
+      <Section size='300px' />
+      <Section />
+    </Layout>
+  ),
+  fixedCol: (
+    <Layout>
+      <Section size='300px' />
+      <Section />
+    </Layout>
+  ),
+  rows: (
+    <Layout type='rows'>
+      <Row />
+      <Row />
+    </Layout>
+  ),
+  columns: (
+    <Layout type='columns'>
+      <Col />
+      <Col />
+    </Layout>
+  ),
+  width: (
+    <Layout>
+      <Section gravity={-1} width={300} />
+      <Section />
+    </Layout>
+  ),
+  height: (
+    <Layout type='rows'>
+      <Section gravity={-1} height={300} />
+      <Section />
+    </Layout>
+  ),
+  common: (
+    <Layout type='rows'>
+      <Section size='80px' />
+      <Section>
+        <Layout>
+          <Section size='20%' />
+          <Section>
+            <Layout type='rows' mode='spaced'>
+              <Section>
+                <Layout mode='spaced'>
+                  <Section size='300px' />
+                  <Section />
+                  <Section size='300px' />
+                </Layout>
+              </Section>
+              <Section />
+              <Section />
+            </Layout>
+          </Section>
+        </Layout>
+      </Section>
+    </Layout>
+  ),
+  singleLayout: (
+    <Layout />
+  ),
+  singleSection: (
+    <Section />
+  ),
+  tenColumns: (
+    <Layout>
+      <Section />
+      <Section />
+      <Section size='20px' />
+      <Section />
+      <Section />
+      <Section />
+      <Section />
+      <Section />
+      <Section />
+      <Section />
+    </Layout>
+  ),
+  withContent: (
+    <Layout>
+      <Section size='250px' />
+      <Section>
+        <Content />
+      </Section>
+    </Layout>
+  )
+}
 
 ReactDOM.render(
-	cases.rows,
-	main
-);
+  cases.withContent,
+  main
+)
 
-export default cases;
+export default cases
--- src/ProperLayout.jsx
+++ src/ProperLayout.jsx
@@ -1,10 +1,10 @@
-'use strict';
+'use strict'
 
-import Layout from './components/Layout';
-import Section from './components/Section';
-import Row from './components/Row';
-import Col from './components/Col';
+import Layout from './components/Layout'
+import Section from './components/Section'
+import Row from './components/Row'
+import Col from './components/Col'
 
-import './styles/main';
+import './styles/main'
 
-export { Layout, Section, Row, Col };
+export { Layout, Section, Row, Col }
