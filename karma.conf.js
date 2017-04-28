af8601df2591848453896508cf3c028dafd9e984
orballo <asrielo@gmail.com>
2017-04-28 10:37:32 +0200

    Implemented borders as a prop for Layout

--- karma.conf.js
+++ karma.conf.js
@@ -58,7 +58,7 @@ module.exports = function(config) {
 
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		browsers: ['PhantomJS'], // You can also add Chrome
+		browsers: ['PhantomJS', 'Chrome'], // Usually PhantomJS and Chrome
 
 
 		// Continuous Integration mode
--- test/Section.test.jsx
+++ test/Section.test.jsx
@@ -1,125 +1,127 @@
-'use strict';
+/* global expect, it, describe */
 
-import React from 'react';
-import { shallow, mount } from 'enzyme';
-import { Section } from '../src/ProperLayout';
+'use strict'
+
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import { Section } from '../src/ProperLayout'
 
 describe('Layout', () => {
-	describe('Shallow <Section />', () => {
-		const wrapper = shallow(<Section />);
+  describe('Shallow <Section />', () => {
+    const wrapper = shallow(<Section />)
 
-		it("returns a <div>", () => {
-			expect(wrapper.is('div')).to.be.true;
-		});
+    it('returns a <div>', () => {
+      expect(wrapper.is('div')).to.be.true
+    })
 
-		it("has class='section'", () => {
-			expect(wrapper.hasClass('section')).to.be.true;
-		});
+    it("has class='section'", () => {
+      expect(wrapper.hasClass('section')).to.be.true
+    })
 
-		it("has key='section--*'", () => {
-			expect(wrapper.key()).to.match(/^section--[\w-]+$/);
-		});
-	});
+    it("has key='section--*'", () => {
+      expect(wrapper.key()).to.match(/^section--[\w-]+$/)
+    })
+  })
 
-	describe("Mounted <Section />", () => {
-		const wrapper = mount(<Section />);
+  describe('Mounted <Section />', () => {
+    const wrapper = mount(<Section />)
 
-		it("has props.type='columns'", () => {
-			expect(wrapper.prop('type')).to.equal('columns');
-		});
+    it("has props.type='columns'", () => {
+      expect(wrapper.prop('type')).to.equal('columns')
+    })
 
-		it("has props.mode='default'", () => {
-			expect(wrapper.prop('mode')).to.equal('default');
-		});
+    it("has props.mode='default'", () => {
+      expect(wrapper.prop('mode')).to.equal('default')
+    })
 
-		it("has props.index=0", () => {
-			expect(wrapper.prop('index')).to.equal(0);
-		});
+    it('has props.index=0', () => {
+      expect(wrapper.prop('index')).to.equal(0)
+    })
 
-		it("has state.key='section--*'", () => {
-			expect(wrapper.state('key')).to.match(/^section--[\w-]+$/);
-		});
+    it("has state.key='section--*'", () => {
+      expect(wrapper.state('key')).to.match(/^section--[\w-]+$/)
+    })
 
-		it("has state.className='section'", () => {
-			expect(wrapper.state('className')).to.equal('section');
-		});
+    it("has state.className='section'", () => {
+      expect(wrapper.state('className')).to.equal('section')
+    })
 
-		it("has state.width=undefined", () => {
-			expect(wrapper.state('width')).to.be.undefined;
-		});
+    it('has state.width=undefined', () => {
+      expect(wrapper.state('width')).to.be.undefined
+    })
 
-		it("has state.height='100%'", () => {
-			expect(wrapper.state('height')).to.equal('100%');
-		});
+    it("has state.height='100%'", () => {
+      expect(wrapper.state('height')).to.equal('100%')
+    })
 
-		it("has class='section'", () => {
-			expect(wrapper.hasClass('section')).to.be.true;
-		});
+    it("has class='section'", () => {
+      expect(wrapper.hasClass('section')).to.be.true
+    })
 
-		let styles = wrapper.find('div').prop('style');
+    let styles = wrapper.find('div').prop('style')
 
-		it("has style.width=undefined", () => {
-			expect(styles.width).to.be.undefined;
-		});
+    it('has style.width=undefined', () => {
+      expect(styles.width).to.be.undefined
+    })
 
-		it("has style.height='100%'", () => {
-			expect(styles.height).to.equal('100%');
-		});
+    it("has style.height='100%'", () => {
+      expect(styles.height).to.equal('100%')
+    })
 
-		it("has style.left='0%'", () => {
-			expect(styles.left).to.equal('0%');
-		});
-	});
+    it("has style.left='0%'", () => {
+      expect(styles.left).to.equal('0%')
+    })
+  })
 
-	describe("Mounted <Section size='100px' />", () => {
-		const wrapper = mount(<Section size='100px' />);
+  describe("Mounted <Section size='100px' />", () => {
+    const wrapper = mount(<Section size='100px' />)
 
-		it("has props.size='100px'", () => {
-			expect(wrapper.prop('size')).to.equal('100px');
-		});
+    it("has props.size='100px'", () => {
+      expect(wrapper.prop('size')).to.equal('100px')
+    })
 
-		it("has state.width='100px'", () => {
-			expect(wrapper.state('width')).to.equal('100px');
-		});
+    it("has state.width='100px'", () => {
+      expect(wrapper.state('width')).to.equal('100px')
+    })
 
-		it("has state.height='100%'", () => {
-			expect(wrapper.state('height')).to.equal('100%');
-		});
+    it("has state.height='100%'", () => {
+      expect(wrapper.state('height')).to.equal('100%')
+    })
 
-		let styles = wrapper.find('div').prop('style');
+    let styles = wrapper.find('div').prop('style')
 
-		it("has style.width='100px'", () => {
-			expect(styles.width).to.equal('100px');
-		});
+    it("has style.width='100px'", () => {
+      expect(styles.width).to.equal('100px')
+    })
 
-		it("has style.height='100%'", () => {
-			expect(styles.height).to.equal('100%');
-		});
-	});
+    it("has style.height='100%'", () => {
+      expect(styles.height).to.equal('100%')
+    })
+  })
 
-	describe("Mounted <Section size='50%' />", () => {
-		const wrapper = mount(<Section size='50%' />);
+  describe("Mounted <Section size='50%' />", () => {
+    const wrapper = mount(<Section size='50%' />)
 
-		it("has props.size='50%'", () => {
-			expect(wrapper.prop('size')).to.equal('50%');
-		});
+    it("has props.size='50%'", () => {
+      expect(wrapper.prop('size')).to.equal('50%')
+    })
 
-		it("has state.width='50%'", () => {
-			expect(wrapper.state('width')).to.equal('50%');
-		});
+    it("has state.width='50%'", () => {
+      expect(wrapper.state('width')).to.equal('50%')
+    })
 
-		it("has state.height='100%'", () => {
-			expect(wrapper.state('height')).to.equal('100%');
-		});
+    it("has state.height='100%'", () => {
+      expect(wrapper.state('height')).to.equal('100%')
+    })
 
-		let styles = wrapper.find('div').prop('style');
+    let styles = wrapper.find('div').prop('style')
 
-		it("has style.width='50%'", () => {
-			expect(styles.width).to.equal('50%');
-		});
+    it("has style.width='50%'", () => {
+      expect(styles.width).to.equal('50%')
+    })
 
-		it("has style.height='100%'", () => {
-			expect(styles.height).to.equal('100%');
-		});
-	});
-});
+    it("has style.height='100%'", () => {
+      expect(styles.height).to.equal('100%')
+    })
+  })
+})
